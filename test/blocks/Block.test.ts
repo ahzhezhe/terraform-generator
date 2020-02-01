5aaa9820286ac97cddc6bb982a6e9668118c2abc
Ah Zhe <zhejiet@hotmail.com>
2020-02-02 02:35:29 +0800

    update test

--- test/blocks/Block.test.ts
+++ test/blocks/Block.test.ts
@@ -1,18 +1,18 @@
 import { Resource } from '../..';
-import { tfGenerator12, arg4 } from '.';
+import { tfGenerator12 } from '.';
 
 test('Block identifier', () => {
-  expect(() => new Resource(tfGenerator12, '!@#', '$%^', arg4(tfGenerator12))).toThrow();
+  expect(() => new Resource(tfGenerator12, '!@#', '$%^')).toThrow();
 });
 
 test('Block arguments', () => {
-  const resource = new Resource(tfGenerator12, 'type', 'name', arg4(tfGenerator12));
+  const resource = new Resource(tfGenerator12, 'type', 'name', { a: 1 });
   expect(resource.getArguments()).toMatchSnapshot();
-  expect(resource.setArgument('test0', 'test0').getArguments()).toMatchSnapshot();
+  expect(resource.setArgument('b', 2).getArguments()).toMatchSnapshot();
   expect(resource.setArguments({
-    test1: 'test1',
-    test2: 2,
-    test3: true
+    a: 0,
+    b: 1,
+    c: 2
   }).getArguments()).toMatchSnapshot();
-  expect(resource.deleteArgument('test2').getArguments()).toMatchSnapshot();
+  expect(resource.deleteArgument('b').getArguments()).toMatchSnapshot();
 });
