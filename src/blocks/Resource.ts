6e58002b71604b93abefe703069193c147d948b3
Chang Zhe Jiet <zhejiet.chang@ufinity.com>
2020-02-11 12:46:59 +0800

    toDataSourceByTags

--- src/blocks/Resource.ts
+++ src/blocks/Resource.ts
@@ -1,4 +1,4 @@
-import { Block, Argument, Attribute } from '..';
+import { Block, Argument, Attribute, DataSource, Map } from '..';
 
 export default class Resource extends Block {
 
@@ -26,6 +26,35 @@ export default class Resource extends Block {
 
   getAttribute(name: string): Attribute {
     return new Attribute(this, name);
+  }
+
+  /**
+   * Convert resource into data source, filtered by resource's tags
+   * 
+   * @param name data source name, put null to use the same name as resource
+   */
+  toDataSourceByTags(name?: string): DataSource {
+    if (!name) {
+      name = this.name;
+    }
+
+    const tags = this.getArgument('tags');
+    if (!tags) {
+      throw new Error('Resource does not have tags.');
+    }
+    if (!(tags instanceof Map)) {
+      throw new Error('Tags is not a Map.');
+    }
+
+    const filter = [];
+    for (const tagName in tags.args) {
+      filter.push({
+        name: `tag:${tagName}`,
+        values: [tags.args[tagName]]
+      });
+    }
+
+    return new DataSource(this.type, name, { filter });
   }
 
 }
--- test/blocks/Resource.test.ts
+++ test/blocks/Resource.test.ts
@@ -1,4 +1,4 @@
-import { Resource } from '../../src';
+import { Resource, map } from '../../src';
 import { arg4 } from '..';
 
 test('Resource', () => {
@@ -7,4 +7,31 @@ test('Resource', () => {
   expect(resource.toTerraform('0.12')).toMatchSnapshot();
   expect(resource.asArgument().toTerraform()).toBe('type.name');
   expect(resource.getAttribute('attr').toTerraform()).toBe('type.name.attr');
+});
+
+test('toDataSourceByTags', () => {
+  const resource = new Resource('type', 'name', {
+    tags: map({
+      a: 'a',
+      b: 'b'
+    })
+  });
+  expect(resource.toDataSourceByTags().toTerraform('0.11')).toMatchSnapshot();
+  expect(resource.toDataSourceByTags().toTerraform('0.12')).toMatchSnapshot();
+  expect(resource.toDataSourceByTags('newName').toTerraform('0.11')).toMatchSnapshot();
+  expect(resource.toDataSourceByTags('newName').toTerraform('0.12')).toMatchSnapshot();
+});
+
+test('toDataSourceByTags no tags', () => {
+  const resource = new Resource('type', 'name');
+  expect(() => resource.toDataSourceByTags()).toThrow();
+  expect(() => resource.toDataSourceByTags('newName')).toThrow();
+});
+
+test('toDataSourceByTags tags not map', () => {
+  const resource = new Resource('type', 'name', {
+    tags: 'tags'
+  });
+  expect(() => resource.toDataSourceByTags()).toThrow();
+  expect(() => resource.toDataSourceByTags('newName')).toThrow();
 });
