066ab08d7313b10e1b27889cf6994a98fba71e1e
Chang Zhe Jiet <zhejiet.chang@ufinity.com>
2020-07-17 16:14:56 +0800

    test arg in interpolation

--- test/arguments/Argument.test.ts
+++ test/arguments/Argument.test.ts
@@ -25,3 +25,12 @@ test('arg', () => {
   expect(arg(attr, true).toTerraform()).toBe('type.name.attr');
   expect(arg(attr, false).toTerraform()).toBe('type.name.attr');
 });
+
+test('arg in interpolation', () => {
+  expect(arg(`prefix-${arg('x')}-suffix`).toTerraform()).toBe('prefix-${x}-suffix');
+  expect(arg(`prefix-${arg('x', true)}-suffix`).toTerraform()).toBe('prefix-${x}-suffix');
+  expect(arg(`prefix-${arg('x', false)}-suffix`).toTerraform()).toBe('prefix-${x}-suffix');
+  expect(arg(`prefix-${arg(attr)}-suffix`).toTerraform()).toBe('prefix-${type.name.attr}-suffix');
+  expect(arg(`prefix-${arg(attr, true)}-suffix`).toTerraform()).toBe('prefix-${type.name.attr}-suffix');
+  expect(arg(`prefix-${arg(attr, false)}-suffix`).toTerraform()).toBe('prefix-${type.name.attr}-suffix');
+});
