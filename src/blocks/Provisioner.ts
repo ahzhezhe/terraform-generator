ec715f46d3c10daf521c21e33c6fe0137a5d8f57
Chang Zhe Jiet <zhejiet.chang@ufinity.com>
2020-07-18 12:10:40 +0800

    test

--- src/blocks/Provisioner.ts
+++ src/blocks/Provisioner.ts
@@ -18,7 +18,7 @@ export default class Provisioner extends Block {
   }
 
   asArgument(): Argument {
-    return new Argument(JSON.stringify(this.type), true);
+    throw new Error('Inaccessible function.');
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
